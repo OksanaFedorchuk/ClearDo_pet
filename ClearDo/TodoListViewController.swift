e10220e7758c95bd632bb30d29c2779f0b4dc8df
Oksana Fedorchuk <klassi4eske@gmail.com>
2019-12-02 15:14:58 +0200

    1. LaunchScreen Storyboard + Table View Controller + TableView data source methods

---
+++ ClearDo/TodoListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ClearDo
+//
+//  Created by Oksana on 25.11.2019.
+//  Copyright © 2019 Oksana Fedorchuk. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Dog", "Cat", "Donkey"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // TableView data source methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+
+}
+
